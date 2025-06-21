db9419632427034e2b68377684002dd139c265c1
fenyadim <dimos9813@gmail.com>
2025-06-21 23:34:44 +0800

    fix: Зафиксил баги

--- jest.config.ts
+++ jest.config.ts
@@ -4,15 +4,15 @@ const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./setup-jest.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '.*\\.spec\\.(js|jsx|ts|tsx)$']
-  // transform: {
-  //   '^.+\\.tsx?$': [
-  //     'ts-jest',
-  //     {
-  //       tsconfig: 'tsconfig.jest.json'
-  //     }
-  //   ]
-  // }
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '.*\\.spec\\.(js|jsx|ts|tsx)$'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json'
+      }
+    ]
+  }
 }
 
 export default config
